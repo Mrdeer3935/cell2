3cf7d2c5eed689acc0891032e992c6936c26fc61
Mrdeer3935 <105971210+Mrdeer3935@users.noreply.github.com>
2023-04-09 01:19:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import hydralit_components as hc
 from hydralit import HydraApp
 from Browse import BrowseAPP
 from Home import HomeAPP
@@ -20,10 +19,10 @@ if __name__ == '__main__':
         title='XX SingleCell RNAseq Database',
         hide_streamlit_markers=False,
         # add a nice banner, this banner has been defined as 5 sections with spacing defined by the banner_spacing array below.
-        use_banner_images=["logo.png", None, {
+        use_banner_images=[None,"logo.png" , {
             'header': "<h1 style='text-align:center;padding: 0px 0px;color:black;font-size:300%;'>XX SingleCell RNAseq Database</h1><br>"},
                            None, None],
-        banner_spacing=[5, 10, 60, 10, 5],
+        banner_spacing=[10, 3, 30, 3, 10],
         use_navbar=True,
         navbar_sticky=True,
         navbar_animation=True,
@@ -36,6 +35,5 @@ if __name__ == '__main__':
 
 
     app.run()
-
 
 
