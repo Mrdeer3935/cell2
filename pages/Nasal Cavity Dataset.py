2cde2b2f84a67ddf002ba53e68d8cc25918586dd
Mrdeer3935 <105971210+Mrdeer3935@users.noreply.github.com>
2023-04-12 21:05:23 +0800

    Update Nasal Cavity Dataset.py

--- pages/Nasal Cavity Dataset.py
+++ pages/Nasal Cavity Dataset.py
@@ -2,6 +2,7 @@ import streamlit as st
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 import pandas as pd
 import numpy as np
+import hydralit_components as hc
 
 st.set_page_config(page_title='Nasal Cavity Dataset',
                    page_icon=':nose:',layout='wide',initial_sidebar_state='collapsed',)
@@ -23,3 +24,24 @@ with col2:
 
         st.markdown("# MORE INFORTION"
                     "数据下载链接等")
+        hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good', bar_value=100,key=1)
+
+
+    with GEO:
+        st.markdown('##### Data list:')
+        option = st.selectbox(
+            '',
+            ('GSE184117',
+             'GSE182475',
+             'GSE139522',
+             'GSE121600',
+             'GSE191061',
+             'GSE155113',
+             'GSE171488',
+             'GSE164547',
+             'GSE162864',
+             'GSE103518'
+             ))
+
+        hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good', bar_value=100,key=2)
+
