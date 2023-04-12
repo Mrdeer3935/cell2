c10c697d282d8f8c3c8dbbb1f2b129433aa8ec80
Mrdeer3935 <105971210+Mrdeer3935@users.noreply.github.com>
2023-04-12 21:08:19 +0800

    Update Help.py

--- Help.py
+++ Help.py
@@ -2,6 +2,7 @@ import streamlit as st
 import hydralit_components as hc
 import datetime
 from hydralit import HydraHeadApp
+import streamlit.components.v1 as components
 
 
 class HelpAPP(HydraHeadApp):
@@ -10,3 +11,8 @@ class HelpAPP(HydraHeadApp):
     def run(self):
         #-------------------existing untouched code------------------------------------------
         st.title('user guide')
+
+        with open('visitors.html', "r", encoding='utf-8') as f:
+            data = f.read()
+        #components.html("", height=50)
+        components.html(data, height=500, scrolling=False)
